8fcec5d3d3151e151d5cb6ea80b6e752dcb7f081
Jaron Truman <joltruman88@gmail.com>
2019-05-06 17:22:34 -0700

    Displaying raw data

--- src/App.js
+++ src/App.js
@@ -4,26 +4,21 @@ import './App.css';
 import Data from './data';
 
 class App extends Component {
-  rawValues() {
-    return Data.routes.map((route, i) => {
-      return (
-        <li key={i}>
-          `airline: ${route.airline} - src: ${route.src} - dest: ${route.dest}`
-        </li>
-      );
-    });
-  }
 
   render() {
-    const values = this.rawValues();
-
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
-          <ul>{values}</ul>
+          {Data.routes.map((route, i) => {
+            return (
+              <li key={i}>
+                Airline - {route.airline}, Source - {route.src}, Destination - {route.dest}
+              </li>
+            );
+          })}
         </section>
       </div>
     );
