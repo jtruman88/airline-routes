2703e5e914e80debb88724766db05109ec7b0f3d
Jaron Truman <joltruman88@gmail.com>
2019-05-06 20:42:20 -0700

    Added 'filter by airline' feature.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      airline: 'all',
+    };
+
     this.formatValue = this.formatValue.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
+
+  handleSelectChange(e) {
+    this.setState({
+      airline: e.target.value,
+    });
   }
 
   formatValue(property, value) {
@@ -25,18 +36,36 @@ class App extends Component {
       {name: 'Destination Airport', property: 'dest'}
     ];
 
+    const filteredRoutes = Data.routes.filter(route => {
+      const airline = Data.getAirlineById(route.airline).name;
+      return airline === this.state.airline || this.state.airline === 'all';
+    });
+
+    const airlineOptions = Data.airlines.map(airline => {
+      return (
+        <option key={airline.id} value={airline.name}>{airline.name}</option>
+      );
+    });
+
+    const perPage = (filteredRoutes.length < 25 ? filteredRoutes.length : 25)
+
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
+          Show routes for
+          <select value={this.state.airline} onChange={this.handleSelectChange}>
+            <option value="all">All Airlines</option>
+            { airlineOptions }
+          </select>
           <Table
             className="routes-table"
             columns={columns}
-            rows={Data.routes}
+            rows={filteredRoutes}
             format={this.formatValue}
-            perPage={25}
+            perPage={perPage}
           />
         </section>
       </div>
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Data from '../data';
 
 class Table extends Component {
   constructor(props) {
