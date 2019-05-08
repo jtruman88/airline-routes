63c149875666f5bbc4c94ecfb0874db78c80e981
Jaron Truman <joltruman88@gmail.com>
2019-05-07 18:11:20 -0700

    Added functionality to disable any options that were not relevant to the current selection.

--- src/App.js
+++ src/App.js
@@ -10,15 +10,29 @@ class App extends Component {
 
     this.state = {
       airline: 'all',
+      airport: 'all',
     };
 
     this.formatValue = this.formatValue.bind(this);
     this.onAirlineSelect = this.onAirlineSelect.bind(this);
+    this.onAirportSelect = this.onAirportSelect.bind(this);
+    this.hasAirport = this.hasAirport.bind(this);
+    this.hasAirline = this.hasAirline.bind(this);
+    this.showAllRoutes = this.showAllRoutes.bind(this);
+    this.availableAirlines = this.availableAirlines.bind(this);
+    this.availableAirports = this.availableAirports.bind(this);
+    this.filterByAirports = this.filterByAirports.bind(this);
   }
 
   onAirlineSelect(airline) {
     this.setState({
       airline: airline,
+    });
+  }
+
+  onAirportSelect(airport) {
+    this.setState({
+      airport: airport,
     });
   }
 
@@ -30,6 +44,75 @@ class App extends Component {
     }
   }
 
+  hasAirline(airline) {
+    return airline === this.state.airline ||
+           this.state.airline === 'all';
+  }
+
+  hasAirport(dest, src) {
+    return (dest === this.state.airport || src === this.state.airport) ||
+            this.state.airport === 'all';
+  }
+
+  showAllRoutes(e) {
+    this.setState({
+      airline: 'all',
+      airport: 'all',
+    });
+  }
+
+  availableAirlines() {
+    if (this.state.airport === 'all') {
+      return Data.airlines.map(airline => airline.name);
+    }
+
+    const airlines = [];
+
+    Data.routes.forEach(route => {
+      if (this.state.airport === Data.getAirportByCode(route.dest).name ||
+          this.state.airport === Data.getAirportByCode(route.src).name) {
+            airlines.push(Data.getAirlineById(route.airline).name);
+      }
+    });
+
+    return airlines;
+  }
+
+  availableAirports() {
+    if (this.state.airline === 'all') {
+      if (this.state.airport === 'all') {
+        return Data.airports.map(airport => airport.name);
+      } else {
+        return this.filterByAirports(Data.routes);
+      }
+    }
+
+    const routes = Data.routes.filter(route => {
+      return Data.getAirlineById(route.airline).name === this.state.airline;
+    });
+
+    if (this.state.airport === 'all') {
+      const dests = routes.map(route => Data.getAirportByCode(route.dest).name);
+      const srcs = routes.map(route => Data.getAirportByCode(route.src).name);
+
+      return [...dests, ...srcs];
+    } else {
+      return this.filterByAirports(routes);
+    }
+  }
+
+  filterByAirports(routes) {
+    const filteredRoutes = routes.filter(route => {
+      return this.state.airport === Data.getAirportByCode(route.dest).name ||
+             this.state.airport === Data.getAirportByCode(route.src).name;
+    });
+
+    const dests = filteredRoutes.map(route => Data.getAirportByCode(route.dest).name);
+    const srcs = filteredRoutes.map(route => Data.getAirportByCode(route.src).name);
+
+    return [...dests, ...srcs];
+  }
+
   render() {
     const columns = [
       {name: 'Airline', property: 'airline'},
@@ -39,10 +122,14 @@ class App extends Component {
 
     const filteredRoutes = Data.routes.filter(route => {
       const airline = Data.getAirlineById(route.airline).name;
-      return airline === this.state.airline || this.state.airline === 'all';
+      const destination = Data.getAirportByCode(route.dest).name;
+      const source = Data.getAirportByCode(route.src).name;
+      return this.hasAirline(airline) && this.hasAirport(destination, source);
     });
 
-    const perPage = (filteredRoutes.length < 25 ? filteredRoutes.length : 25)
+    const perPage = (filteredRoutes.length < 25 ? filteredRoutes.length : 25);
+    const allRoutesShown = this.state.airline === 'all' &&
+                           this.state.airport === 'all';
 
     return (
       <div className="app">
@@ -58,7 +145,21 @@ class App extends Component {
             allTitle="All Airlines"
             value={this.state.airline}
             onSelect={this.onAirlineSelect}
+            enabledValues={this.availableAirlines()}
           />
+          flying in or out of
+            <Select
+              options={Data.airports}
+              valueKey="code"
+              titleKey="name"
+              allTitle="All Airports"
+              value={this.state.airport}
+              onSelect={this.onAirportSelect}
+              enabledValues={this.availableAirports()}
+            />
+          <button onClick={this.showAllRoutes} disabled={allRoutesShown}>
+            Show All Routes
+          </button>
           <Table
             className="routes-table"
             columns={columns}
--- src/components/Select.js
+++ src/components/Select.js
@@ -5,16 +5,33 @@ class Select extends Component {
     super(props);
 
     this.handleSelect = this.handleSelect.bind(this);
+    this.isDisabled = this.isDisabled.bind(this);
   }
 
   handleSelect(e) {
     this.props.onSelect(e.target.value);
   }
 
+  isDisabled(option) {
+    if (this.props.valueKey === 'id') {
+      return (this.props.value !== 'all' && this.props.value !== option.name) ||
+             !this.props.enabledValues.includes(option.name);
+    } else {
+      if (this.props.enabledValues.includes(option.name)) {
+        return false;
+      } else {
+        return this.props.value !== 'all' || this.props.value !== option.name;
+      }
+    }
+  }
+
   render() {
-    let options = this.props.options.map(airline => {
+    let options = this.props.options.map(option => {
       return (
-        <option key={airline.id} value={airline.name}>{airline.name}</option>
+        <option key={option.id || option.code} value={option.name}
+                disabled={this.isDisabled(option)}>
+          {option.name}
+        </option>
       );
     });
 
@@ -30,3 +47,19 @@ class Select extends Component {
 }
 
 export default Select;
+
+/*
+Airlines are disabled if:
+  props.value !== 'all' AND
+  props.value !-- option.name
+  AND routes does not have a dest or src airport with the airline
+    loop through routes checking if getairport.code(route.dest or route.src) === state.airport.
+      add getAirlineById(route.airline) to airlines array.
+
+Airports are disabled if:
+  props.value !== 'all' AND
+  props.value doesn't equal option.name
+  routes does not have an airline with the dest or src airport AND
+  routes does not have a dest or src airport with current airport
+
+*/
